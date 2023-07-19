26d1b859c0b570f53d2ce4bbeaa2e06efd38fc9d
AhmedHamdi0 <ahmed88hamdi99@gmail.com>
2023-07-19 23:00:37 +0300

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,15 +18,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf("is positive");
+printf("%i is positive\n", n);
 }
 else if (n < 0)
 {
-printf("is negative");
+printf("%i is negative\n", n);
 }
 else
 {
-printf("is zero");
+printf("%i is zero\n", n);
 }
 return (0);
 }
