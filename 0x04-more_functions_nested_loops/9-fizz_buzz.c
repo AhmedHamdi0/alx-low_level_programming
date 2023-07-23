8086fad7a69a48169678b2f4aa5b3096ff6045e3
AhmedHamdi0 <ahmed88hamdi99@gmail.com>
2023-07-23 18:17:37 +0300

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,25 +16,31 @@ int main(void)
 int i;
 for (i = 1; i <= 100; i++)
 {
-if (i % 3 == 0 && i % 5 != 0)
+if ((i % 3 == 0) && !(i % 5 == 0))
 {
-printf("Fizz ");
+printf("Fizz");
 }
-else if (i % 3 != 0 && i % 5 == 0)
+else if (!(i % 3 == 0) && (i % 5 == 0))
 {
-printf("Buzz ");
+printf("Buzz");
 }
-else if (i % 3 == 0 && i % 5 == 0)
+else if ((i % 3 == 0) && (i % 5 == 0))
 {
-printf("FizzBuzz ");
+printf("FizzBuzz");
 }
 else
 {
-printf("%i ", i);
+printf("%d", i);
 }
+if (num != 100)
+{
+printf(" ");
 }
+else
+{
 printf("\n");
-
+}
+}
 return (0);
 }
 
