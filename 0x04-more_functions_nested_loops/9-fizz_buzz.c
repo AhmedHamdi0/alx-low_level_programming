af9fe5b73e5470aa4707e9c9c1cc592ccd13051c
AhmedHamdi0 <ahmed88hamdi99@gmail.com>
2023-07-23 18:21:25 +0300

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,46 +1,39 @@
 #include <stdio.h>
 
 /**
- * main - Prints numbers from 1 to 100 with replacements.
- *
- * Description: This function prints numbers from 1 to 100. For multiples of 3,
- * it prints "Fizz," for multiples of 5, it prints "Buzz," and for multiples
- * of both 3 and 5, it prints "FizzBuzz."
- *
- * Return: 0 (on success)
- */
-
+* main - Prints numbers from 1 to 100 with replacements.
+*
+* Description: This function prints numbers from 1 to 100. For multiples of 3,
+* it prints "Fizz," for multiples of 5, it prints "Buzz," and for multiples
+* of both 3 and 5, it prints "FizzBuzz."
+*
+* Return: 0 (on success)
+*/
 
 int main(void)
 {
 int i;
 for (i = 1; i <= 100; i++)
 {
-if ((i % 3 == 0) && !(i % 5 == 0))
+if (i % 3 == 0 && i % 5 != 0)
 {
-printf("Fizz");
+printf("Fizz ");
 }
-else if (!(i % 3 == 0) && (i % 5 == 0))
+else if (i % 3 != 0 && i % 5 == 0)
 {
-printf("Buzz");
+printf("Buzz ");
 }
-else if ((i % 3 == 0) && (i % 5 == 0))
+else if (i % 3 == 0 && i % 5 == 0)
 {
-printf("FizzBuzz");
-}
-else
-{
-printf("%d", i);
-}
-if (num != 100)
-{
-printf(" ");
+printf("FizzBuzz ");
 }
 else
 {
+printf("%i ", i);
+}
+}
 printf("\n");
-}
-}
+
 return (0);
 }
 
