ad7496e895973c6f57f2540da4b4d1e909052b1d
AhmedHamdi0 <ahmed88hamdi99@gmail.com>
2023-07-23 16:30:14 +0300

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,28 +12,27 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
+if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
 {
-if (month > 2)
+if (month >= 2 && day >= 60)
 {
 day++;
 }
+
+printf("Day of the year: %d\n", day);
+printf("Remaining days: %d\n", 366 - day);
 }
 else
 {
-if (month == 2 && day == 29)
+if (month == 2 && day == 60)
 {
-printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
-return;
+printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+}
+else
+{
+printf("Day of the year: %d\n", day);
+printf("Remaining days: %d\n", 365 - day);
 }
 }
-
-int day_of_year = convert_day(month, day);
-int days_in_year = (year % 4 == 0 && (year % 100 != 0
-|| year % 400 == 0)) ? 366 : 365;
-int remaining_days = days_in_year - day_of_year;
-
-printf("Day of the year: %d\n", day_of_year);
-printf("Remaining days: %d\n", remaining_days);
 }
 
