4e1c81cd2ebeaf0ac0b429f04e22209c4183470c
AhmedHamdi0 <ahmed88hamdi99@gmail.com>
2023-07-23 16:59:34 +0300

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* mul - multiplies two integers.
+*
+* Description: This function checks whether the given integer `c`
+*              corresponds to a digit (0 through 9).
+*
+* @a: The first integer value of the multiplication.
+* @b: The second integer value of the multiplication.
+*
+* Return: the result of the multiplication.
+*/
+
+int mul(int a, int b)
+{
+return (a * b);
+}
+
