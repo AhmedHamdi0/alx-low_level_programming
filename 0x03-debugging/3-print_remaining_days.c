62612dd42c526303fda6540ff9e8c80daedc2230
AhmedHamdi0 <ahmed88hamdi99@gmail.com>
2023-07-23 16:38:54 +0300

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -17,31 +17,34 @@ int leap = 0;
 
 if (year % 4 == 0)
 {
-if (year % 100 != 0 || year % 400 == 0)
+if (year % 100 == 0)
+{
+if (year % 400 == 0)
+leap = 1;
+}
+else
 leap = 1;
 }
 
-if (leap && month > 2)
+if (month < 1 || month > 12 || day < 1 || day > days_in_month[month])
 {
-day++;
+printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
+return;
 }
 
-int day_of_year = 0;
+if (leap && month > 2)
+day++;
+
+int day_of_year = day;
 for (int i = 1; i < month; i++)
 {
 day_of_year += days_in_month[i];
 }
-day_of_year += day;
 
-if (leap)
-{
+if (leap && month > 2)
+day_of_year++;
+
 printf("Day of the year: %d\n", day_of_year);
-printf("Remaining days: %d\n", 366 - day_of_year);
-}
-else
-{
-printf("Day of the year: %d\n", day_of_year);
-printf("Remaining days: %d\n", 365 - day_of_year);
-}
+printf("Remaining days: %d\n", leap ? 366 - day_of_year : 365 - day_of_year);
 }
 
