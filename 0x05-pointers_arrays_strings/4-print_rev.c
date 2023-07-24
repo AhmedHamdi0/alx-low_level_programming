913c71f122952c348a1de5779811b98672407683
AhmedHamdi0 <ahmed88hamdi99@gmail.com>
2023-07-24 19:46:08 +0300

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,11 +8,18 @@
 
 void print_rev(char *s)
 {
-int length = _strlen(s);
-for (int i = length - 1; i > 0; i--)
+int length = 0;
+
+while (s[length] != '\0')
+{
+length++;
+}
+
+for (int i = length - 1; i >= 0; i--)
 {
 _putchar(s[i]);
 }
+
 _putchar('\n');
 }
 
