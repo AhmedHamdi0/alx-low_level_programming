b37fbcc501b6c35a345ce7a4d79c2cf3bb58cdb7
Ahmed Hamdi <119865529+AhmedHamdi0@users.noreply.github.com>
2023-08-05 01:26:48 +0300

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 /**
  * main - Entry point of the program
  *
@@ -6,13 +8,12 @@
  *
  * Return: Always 0 (Success)
  */
-#include <stdio.h>
 
 int main(int argc, char *argv[])
 {
-    (void)argc;
+(void)argc;
 
-    printf("%s\n", argv[0]);
+printf("%s\n", argv[0]);
 
-    return (0);
+return (0);
 }
