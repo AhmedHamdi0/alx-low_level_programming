d8fee84ec0908a60b9daf3718d00c67862806b4b
Ahmed Hamdi <119865529+AhmedHamdi0@users.noreply.github.com>
2023-08-09 22:52:51 +0300

    2-strlen_recursion.c

--- 0x08-recursion/main.c
+++ 0x08-recursion/main.c
@@ -2,7 +2,8 @@
 #include "main.h"
 
 int main() {
-//	_puts_recursion("Puts with recursion");
+	_puts_recursion("Puts with recursion");
 	_print_rev_recursion("\nColton Walker");
+	printf("%d\n", _strlen_recursion("Corbin Coleman"));
 	return 0;
 }
