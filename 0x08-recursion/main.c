046bf6e1519cdcbf7f7fb6c6bc9836f3f8d43876
Ahmed Hamdi <119865529+AhmedHamdi0@users.noreply.github.com>
2023-08-09 23:15:13 +0300

    4-pow_recursion.c

--- 0x08-recursion/main.c
+++ 0x08-recursion/main.c
@@ -3,9 +3,9 @@
 
 int main() {
 	_puts_recursion("Puts with recursion");
-	
+
 	_print_rev_recursion("\nColton Walker");
-	
+
 	printf("%d\n", _strlen_recursion("Corbin Coleman"));
 
 	int r;
@@ -18,5 +18,17 @@ int main() {
 	r = factorial(-1024);
 	printf("%d\n", r);
 	
+	r = _pow_recursion(1, 10);
+	printf("%d\n", r);
+	r = _pow_recursion(1024, 0);
+	printf("%d\n", r);
+	r = _pow_recursion(2, 16);
+	printf("%d\n", r);
+	r = _pow_recursion(5, 2);
+	printf("%d\n", r);
+	r = _pow_recursion(5, -2);
+	printf("%d\n", r);
+	r = _pow_recursion(-5, 3);
+	printf("%d\n", r);
 	return (0);
 }
