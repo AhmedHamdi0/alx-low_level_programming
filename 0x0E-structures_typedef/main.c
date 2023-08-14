521cbaa01154ed89371f6fbfc6ac48abf9639264
Ahmed Hamdi <119865529+AhmedHamdi0@users.noreply.github.com>
2023-08-15 01:49:24 +0300

    4-new_dog.c

--- 0x0E-structures_typedef/main.c
+++ 0x0E-structures_typedef/main.c
@@ -2,10 +2,9 @@
 #include "dog.h"
 
 int main() {
-	printf("Hello, World!\n");
-	struct dog my_dog;
+	dog_t *my_dog;
 	
-	init_dog(&my_dog, "Poppy", 3.5, "Bob");
-	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
-	return 0;
+	my_dog = new_dog("Poppy", 3.5, "Bob");
+	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
+	return (0);
 }
