b56ba7957e4382be4d15e984ed928cbba84ebfee
Ahmed Hamdi <119865529+AhmedHamdi0@users.noreply.github.com>
2023-08-28 21:51:56 +0300

    0-print_list.c

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,7 +1,23 @@
-#ifndef SINGLY_LINKED_LISTS_LISTS_H
-#define SINGLY_LINKED_LISTS_LISTS_H
+#ifndef LISTS_H
+#define LISTS_H
 
-int _putchar(char c);
+#include <stddef.h>
 
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 
-#endif /* SINGLY_LINKED_LISTS_LISTS_H */
+size_t print_list(const list_t *h);
+
+#endif /* LISTS_H */
